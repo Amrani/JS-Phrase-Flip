785347ee53ac9b407fad83c51848aa5cba1f7d27
David Amrani <davidamrani@MacBook-Pro.attlocal.net>
2016-01-07 22:58:47 -0800

    init

---
+++ phrase-flip.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  var count = 0;
+  clearTimeout(window.letterTimeout);
+  var phrases = ["First", "Second", "Third"];
+  var numberOfPhrases = phrases.length;
+  function typeIt(){
+    type();
+  }
+  function flipup(){
+    $('.box-text').css('transform', 'scaleY(-1)');
+    $('.box-text').css('-ms-filter', 'flipv');
+    $('.box-text').css('filter', 'flipv');
+  }
+  function flipdown(){
+    $('.box-text').text(phrases[count % numberOfPhrases]);
+    $('.box-text').css('transform', 'scaleY(1)');
+    $('.box-text').css('-ms-filter', 'flipv');
+    $('.box-text').css('filter', 'flipv');
+  }
+  function type(){
+    window.letterTimeout = setTimeout(type, 3000);
+    setTimeout(flipup, 700);
+    setTimeout(flipdown, 800);
+    count++;
+  }
+  typeIt(phrases[0]);
+});
